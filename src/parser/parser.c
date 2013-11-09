11a28387951603eabaa395b4eac7aeedfae5df9a
Emmanuel Guet <guet_e@epita.fr>
2013-11-09 15:10:17 +0100

    Parsing an input is properly working

--- src/parser/parser.c
+++ src/parser/parser.c
@@ -31,7 +31,7 @@ static int parse_list(s_token **tok)
         g_global->current_node = g_global->current_node->father;
     return 0;
 }
-
+a
 static int parse_input(void)
 {
     s_token *tok = get_token(INPUT);
@@ -51,7 +51,7 @@ static int parse_input(void)
         return 0;
     }
 }
-a
+
 int parse(void)
 {
     return parse_input();
