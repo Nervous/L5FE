5858ca6e6f7868c06a59613a0fd538b598e2ce2a
Guillaume Moizan <moizan_g@epita.fr>
2013-11-12 08:39:32 +0100

    Minor test.py update

--- tests/test.py
+++ tests/test.py
@@ -5,12 +5,20 @@ test_file = open("tests", "r")
 #test_list is the content of test_file line by line
 test_list = test_file.readlines()
 
+#Variables used during test with test category, command to execute,
+#expected output and expected return value
+category = "" 
+command = ""
+output = ""
+return_value = -1
+
 for line in test_list:
-    print(line)
+    if line != "\n":
+        print(line)
 
 #Calls "echo test" in the shell
 subprocess.call(["echo", "test"])
-# if "echo test" 
+# if "echo test" output is equal to "test\n", prints True 
 if "test\n" == subprocess.check_output(["echo", "test"]):
     print(True)
 else:
