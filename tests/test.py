90f146a86c407e516732fa0b8081c212f647a021
Guillaume Moizan <moizan_g@epita.fr>
2013-11-12 23:14:40 +0100

    Added categories to test script

--- tests/test.py
+++ tests/test.py
@@ -7,6 +7,8 @@ test_list = test_file.readlines()
 
 #Variables used during test with test category, command to execute,
 #expected output and expected return value
+succ_tests = 0
+tests = 0
 category = "" 
 command = ""
 output = ""
@@ -16,6 +18,7 @@ for line in test_list:
     
     if line == "\n": #command to execute?
         if command != "":
+           tests += 1
            cmd = command.split()
            cmd.insert(0, './42sh') #getting from 'cmd' to './42sh cmd'
            # if no ouput specified or if it matches
@@ -24,6 +27,7 @@ for line in test_list:
                if (subprocess.call(cmd,stdout=subprocess.PIPE, \
                         stderr=subprocess.PIPE) == return_value):
                    print(command[:-1] + ': SUCCESS')
+                   succ_tests += 1
                else:
                    print(command[:-1] + ': FAILURE -> Unexpected return value')
            else:
@@ -38,5 +42,14 @@ for line in test_list:
         return_value = int(line[3:])
     elif line[:3] == "OP=":
         output = line[3:]
+    elif line[:4] == "CAT=":
+        if category != "":
+            print(category + ' -> Successful tests : ' + str(succ_tests) +\
+                    '/' + str(tests))
+        category = line[4:-1]
+        tests = 0
+        succ_tests = 0
 
+if category != "":
+    print(category + '-> Successful tests : ' + str(succ_tests) + '/' + str(tests))
 test_file.close()
