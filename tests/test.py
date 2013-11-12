fed82926cb138dc2e38a7458ffe8d64d2c98e42b
Guillaume Moizan <moizan_g@epita.fr>
2013-11-12 08:33:02 +0100

    Added tests file used only for testing test routine for the moment, update on test.py

--- tests/test.py
+++ tests/test.py
@@ -1,1 +1,19 @@
-print("hello, world")
+import subprocess
+
+#Opening the file in read only
+test_file = open("tests", "r")
+#test_list is the content of test_file line by line
+test_list = test_file.readlines()
+
+for line in test_list:
+    print(line)
+
+#Calls "echo test" in the shell
+subprocess.call(["echo", "test"])
+# if "echo test" 
+if "test\n" == subprocess.check_output(["echo", "test"]):
+    print(True)
+else:
+    print(False)
+
+test_file.close()
