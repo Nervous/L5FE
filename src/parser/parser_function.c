dec0d41a116c750a6cad74e0fdc0c23757821477
Antoine Vallee <vallee_a@epita.fr>
2013-11-16 18:20:00 +0100

    AST functions implemented

--- src/parser/parser_function.c
+++ src/parser/parser_function.c
@@ -1,26 +1,31 @@
 #include "parser.h"
 
+extern s_global *g_global;
 int parse_function(s_token **tok)
 {
+    ast_add_step("Funcdec");
     if ((*tok)->type != FUNC)
         return -1;
     *tok = eat_token(*tok);
     *tok = get_token(WORD);
+    climb_ast(1);
     if ((*tok)->type != WORD)
         parse_error("PARSE ERROR : Expected a WORD");
     *tok = eat_token(*tok);
     *tok = get_token(LEFT_PAR);
-
+    climb_ast(1);
     if ((*tok)->type != LEFT_PAR)
         parse_error("PARSE ERROR : Expected a '('");
     *tok = eat_token(*tok);
     *tok = get_token(RIGHT_PAR);
 
+    climb_ast(1);
     if ((*tok)->type != RIGHT_PAR)
         parse_error("PARSE ERROR : Expected a ')'");
     *tok = eat_token(*tok);
     *tok = get_token(EOL);
 
+    climb_ast(1);
     parse_loop_EOL(tok);
     if (parse_shellcommand(tok) == -1)
         parse_error("PARSE ERROR : Expected a shell command !");
