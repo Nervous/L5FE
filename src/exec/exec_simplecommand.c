f9f1730cdb38400426302e89aa69345754085e67
Emmanuel Guet <guet_e@epita.fr>
2013-11-17 16:01:33 +0100

    Redirections are fixed ; Prefixes THEN elements are fixed

--- src/exec/exec_simplecommand.c
+++ src/exec/exec_simplecommand.c
@@ -3,6 +3,5 @@
 int exec_simplecommand(s_list *simple_command)
 {
     char **argv = build_argv(simple_command->node->son_list);
-    return do_fork(char **argv);
-    // CHAR **ARGV NOT FREE /!\
+    return do_fork(argv);
 }
