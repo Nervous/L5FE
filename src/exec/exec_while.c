b7e70e3dd802aba103ea1e2ad1b4c36e136dd54d
Antoine Vallee <vallee_a@epita.fr>
2013-11-17 16:09:59 +0100

    While exec added

---
+++ src/exec/exec_while.c
@@ -0,0 +1,9 @@
+#include "exec.h"
+
+int exec_while(s_list *while_node)
+{
+    int condition = 0;
+    while (condition == 0)
+        condition = exec_compound(while_node->brothers)
+    return exec_dogroup(while_node->brothers->brothers);
+}
