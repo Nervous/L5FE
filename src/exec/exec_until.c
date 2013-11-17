90223c1f1fa47e2748a7c70f706c9a6fe5a75667
Antoine Vallee <vallee_a@epita.fr>
2013-11-17 23:05:00 +0100

    Rule until fixed

--- src/exec/exec_until.c
+++ src/exec/exec_until.c
@@ -3,7 +3,12 @@
 int exec_until(s_list *until)
 {
     int condition = 1;
+    int ret = 0;
     while (condition != 0)
-        condition = exec_compound(until->brothers);
-    return exec_dogroup(until->brothers->brothers);
+    {
+        condition = exec_compound(until->brothers->node->son_list);
+
+        if (condition != 0)
+            ret = exec_dogroup(until->brothers->brothers->node->son_list);
+    }
 }
