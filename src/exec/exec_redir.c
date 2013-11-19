48fea2dee3fc7345a55e92ab53cedfc748927fcf
Amaury Pruvot <pruvot_a@epita.fr>
2013-11-19 14:13:28 +0100

    must rework redir

--- src/exec/exec_redir.c
+++ src/exec/exec_redir.c
@@ -2,25 +2,27 @@
 
 int exec_redir(s_list *ast)
 {
-    switch (ast->node->str)
+    ast=ast;
+
+/*    switch (ast->node->str)
     {
     case ">":
-        return exec_redir_output();
+        return -1;//exec_redir_output();
     case ">|":
-        return exec_redir_output();
+        return -1;//exec_redir_output();
     case "<":
-        return exec_redir_input();
+        return -1;//exec_redir_input();
     case ">>":
-        return exec_redir_appendout();
+        return -1;//exec_redir_appendout();
     case "<<":
-        return exec_redir_heredoc();
+        return -1;//exec_redir_heredoc();
     case "<&":
-        return exec_redir_dupin();
+        return -1;//exec_redir_dupin();
     case ">&":
-        return exec_redir_dupout();
+        return -1;//exec_redir_dupout();
     case "<>":
-        return exec_redir_rw();
-    default:
+        return -1;//exec_redir_rw();
+        default:*/
         return -1;
-    }
+        //}
 }
