97f60cd03605b9047749238fe87b83ec689c240a
Amaury Pruvot <pruvot_a@epita.fr>
2013-11-19 14:10:50 +0100

    parsing redirection types

--- src/exec/exec_redir.c
+++ src/exec/exec_redir.c
@@ -1,7 +1,26 @@
 #include "exec.h"
 
-int exec_redir(s_list *redirection)
+int exec_redir(s_list *ast)
 {
-    redirection = redirection;   //TODO
-    return -1;
+    switch (ast->node->str)
+    {
+    case ">":
+        return exec_redir_output();
+    case ">|":
+        return exec_redir_output();
+    case "<":
+        return exec_redir_input();
+    case ">>":
+        return exec_redir_appendout();
+    case "<<":
+        return exec_redir_heredoc();
+    case "<&":
+        return exec_redir_dupin();
+    case ">&":
+        return exec_redir_dupout();
+    case "<>":
+        return exec_redir_rw();
+    default:
+        return -1;
+    }
 }
