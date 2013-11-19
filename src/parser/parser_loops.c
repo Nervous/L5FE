b8e5ebeff1ce0b82049e96b38784d8c5f669322c
Emmanuel Guet <guet_e@epita.fr>
2013-11-19 18:00:55 +0100

    Finished doxygen of parser

--- src/parser/parser_loops.c
+++ src/parser/parser_loops.c
@@ -1,6 +1,10 @@
 #include "parser.h"
 
 extern s_global *g_global;
+
+/**
+** @brief Eat tokens until get_token doesn't give a EOL anymore
+*/
 void parse_loop_EOL(s_token **tok)
 {
     while ((*tok)->type == EOL)
@@ -9,6 +13,10 @@ void parse_loop_EOL(s_token **tok)
         *tok = get_token(EOL);
     }
 }
+
+/**
+** @brief Eat tokens until get_token doesn't give a WORD anymore
+*/
 
 void parse_loop_WORD(s_token **tok)
 {
