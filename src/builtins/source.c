4f06a7600384995ed67f0fc90ba2c14647ef602c
Antoine Vallee <vallee_a@epita.fr>
2013-11-21 15:02:55 +0100

    Fix compil

--- src/builtins/source.c
+++ src/builtins/source.c
@@ -5,9 +5,10 @@ extern s_global *g_global;
 int my_source(s_list *ast)
 {
     int ret = 0;
-    s_list *tmp_save = g_global->current_node;
+/*    s_list *tmp_save = g_global->current_node;
     get_file(ast->node->str, true);
-    exec_input(get_root(g_global->current_node));
+    exec_input(get_root(g_global->current_node));*/
+    ast = ast;
     return ret;
 }
 
