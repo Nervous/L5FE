d9d5cbf5c0389a61eab3d7785c45b4cb592fc18e
Guillaume Moizan <moizan_g@epita.fr>
2013-11-21 20:48:26 +0100

    Update tild expansion

--- src/expansion/tild_expansion.c
+++ src/expansion/tild_expansion.c
@@ -1,9 +1,46 @@
 #include <stdlib.h>
 #include <string.h>
 
+static int char_counter_slash(char *str)
+{
+    
+}
+
 static int tilde_plus(char **str)
 {
     char *exp = getenv("PWD");
+    int old_len = strlen(*str);
+    int exp_len = strlen(exp); //+1 for '\0' which is not counted in strlen
+    char *new_str = malloc((old_len + exp_len + 1) * sizeof(char));
+    memmove(&(*str[exp_len]), *str, old_len); //makes room for the expansion
+    for (int i = 0; i < exp_len; i++)
+    {
+        new_str[i] = exp[i];
+    }
+    *str = new_str;
+    free(*str);
+    return 0;
+}
+
+static int tilde_plus(char **str)
+{
+    char *exp = getenv("PWD");
+    int old_len = strlen(*str);
+    int exp_len = strlen(exp); //+1 for '\0' which is not counted in strlen
+    char *new_str = malloc((old_len + exp_len + 1) * sizeof(char));
+    memmove(&(*str[exp_len]), *str, old_len); //makes room for the expansion
+    for (int i = 0; i < exp_len; i++)
+    {
+        new_str[i] = exp[i];
+    }
+    *str = new_str;
+    free(*str);
+    return 0;
+}
+
+static int tilde(char **str)
+{
+    char *exp = getenv("HOME");
     int old_len = strlen(*str);
     int exp_len = strlen(exp); //+1 for '\0' which is not counted in strlen
     char *new_str = malloc((old_len + exp_len + 1) * sizeof(char));
@@ -26,7 +63,7 @@ int tilde_handler(char **str)
         else if (*str[1] == '-')
             return tilde_plus(str);
         else
-            return tilde_plus(str);
+            return tilde(str);
     }
     else
         return 1;
