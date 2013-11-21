515993a916b64ca5bad1a847759fec147f1ebf4c
Guillaume Moizan <moizan_g@epita.fr>
2013-11-21 17:58:14 +0100

    tilde ready to be tested

--- src/expansion/tild_expansion.c
+++ src/expansion/tild_expansion.c
@@ -7,7 +7,12 @@ int tilde_plus(char **str)
     int old_len = strlen(*str);
     int exp_len = strlen(exp); //+1 for '\0' which is not counted in strlen
     *str = realloc(*str, (old_len + exp_len + 1) * sizeof(char));
-    
+    memmove(&(*str[exp_len]), *str, old_len); //makes room for the expansion
+    for (int i = 0; i < exp_len; i++)
+    {
+        *str[i] = exp[i];
+    }
+    return 0;
 }
 
 int tilde_handler(char **str)
@@ -15,9 +20,9 @@ int tilde_handler(char **str)
     if (str != NULL && *str != NULL)
     {
         if (str[1] == '+')
-            return tilde_plus(str);
+            return tilde(str);
         else if (str[1] == '-')
-            return tilde_minus(str);
+            return tilde(str);
         else
             return tilde(str);
     }
