4d09ef1b1c1a5a0c911be74e1254202856a12710
Xavier Gerondeau <xavier.gerondeau@epita.fr>
2013-11-22 17:17:08 +0100

    Fix exit builtin

--- src/builtins/exit.c
+++ src/builtins/exit.c
@@ -1,10 +1,16 @@
 #include "builtins.h"
 #include "../struct.h"
+#include "../readline/history.h"
+#include <curses.h>
+#include <term.h>
+#include <unistd.h>
 
 extern s_global *g_global;
 
 void exit_builtin(int n)
 {
+    write_history();
+    tcsetattr(STDIN_FILENO, TCSANOW, &(g_global->attribute));
     if (n == -1)
         exit(1);
     else
