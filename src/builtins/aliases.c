72cadac211540603fe812adbcdb490c1a89f092b
Antoine Vallee <vallee_a@epita.fr>
2013-11-25 14:33:18 +0100

    Alias fixed

--- src/builtins/aliases.c
+++ src/builtins/aliases.c
@@ -8,21 +8,25 @@ extern s_global *g_global;
 int my_alias(s_list *ast)
 {
     char *value = NULL;
+    value = malloc(sizeof (char) * strlen(ast->node->str) + 1);
     char *name = NULL;
+    name = malloc(sizeof (char) * strlen(ast->node->str) + 1);
     if (ast)
-        name = ast->node->str;
+        name = strcpy(name, ast->node->str);
     else
         return 0;
 
     if (!ast->brothers)
         return 0;
-    s_list *tmp = ast->brothers->brothers;
+    s_list *tmp = ast->brothers;
     while (tmp)
     {
+        value = realloc(value, sizeof (char) * (strlen(value)
+                             + strlen(tmp->node->str) + 1));
         if (value)
             value = strcat(value, tmp->node->str);
         else
-            value = tmp->node->str;
+            value = strcpy(value, tmp->node->str);
         tmp = tmp->brothers;
     }
     if (name && value)
@@ -33,8 +37,10 @@ int my_alias(s_list *ast)
 int my_unalias(s_list *ast)
 {
     char *name = NULL;
+    if (!(name = malloc(sizeof (char) * strlen(ast->node->str) + 1)))
+        return -1;
     if (ast)
-        name = ast->node->str;
+        name = strcpy(name, ast->node->str);
     else
         return 0;
     unalias(name);
