77bfec81162cab0bf2ef26a6a24450b7c53bfebb
Antoine Vallee <vallee_a@epita.fr>
2013-11-28 17:28:14 +0100

    Echo huge update on variables / quoting

--- src/builtins/echo.c
+++ src/builtins/echo.c
@@ -5,6 +5,7 @@
 #include "../expansion/expansion.h"
 #include "../fnmatch/my_fnmatch.h"
 
+extern s_global *g_global;
 static void fill_buf(s_list *ast, int j, char *buf, int *trail)
 {
     for (int i = 0; ast->node->str[i]; i++)
@@ -49,6 +50,48 @@ static int expanded_echo(s_list *ast)
     return 0;
 }
 
+static void remove_quotes(s_list *ast)
+{
+    int count = strlen(ast->node->str);
+    for (int i = 0; i < count - 2; i++)
+    {
+        ast->node->str[i] = ast->node->str[i + 1];
+    }
+    ast->node->str[count - 2] = '\0';
+}
+
+static int cut_var(s_list *ast)
+{
+    char *buf = NULL;
+    if (!(buf = malloc(sizeof (char) * strlen(ast->node->str))))
+        return 1;
+    int j = 0;
+    int found = 0;
+    int found_end = 0;
+    for (int i = 2; ast->node->str[i]; i++)
+    {
+        if (ast->node->str[i] == '{' && !found)
+            found = 1;
+        else if (ast->node->str[i] == '}' && !found_end)
+            found_end = 1;
+        else if (ast->node->str[i] == '\"')
+            break;
+        else
+            buf[j++] = ast->node->str[i];
+    }
+    s_var *var = NULL;
+    if (!(var = malloc(sizeof (s_var))))
+        return 1;
+    buf[j] = '\0';
+    var = search_var(g_global->var, buf);
+    if (!var)
+        return 0;
+    free(ast->node->str);
+    ast->node->str = var->value;
+    free(buf);
+    return 0;
+}
+
 static int print_array(s_list *ast, int option_n)
 {
     if (!ast)
@@ -57,6 +100,10 @@ static int print_array(s_list *ast, int option_n)
     {
         if (strcmp(ast->node->str, "$") == 0)
             expand_var(ast);
+        if (ast->node->str[0] == '\'')
+            remove_quotes(ast);
+        else if (ast->node->str[0] == '\"' && ast->node->str[1] == '$')
+            cut_var(ast);
             tilde_handler(&(ast->node->str));
             my_puts(ast->node->str);
         if (ast->brothers)
