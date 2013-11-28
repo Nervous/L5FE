8aef2bf5c4c5c10791fbe4493cd248939b497b3a
Guillaume Moizan <moizan_g@epita.fr>
2013-11-28 16:28:57 +0100

    fnmatch fix, ready to be used

--- src/fnmatch/my_fnmatch.h
+++ src/fnmatch/my_fnmatch.h
@@ -1,6 +1,8 @@
 #ifndef MY_FNMATCH_H
 # define MY_FNMATCH_H
 
+char **get_dir_list2(char *dir);
 int my_fnmatch(const char *pattern, const char *string);
+void path_exp(char **str);
 
 #endif /* !MY_FNMATCH_H */
